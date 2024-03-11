7658e3e694502e57bdcc73aa40ff767d63559ba2
Max <maxv.visscher@gmail.com>
2024-03-11 13:17:57 +0100

    added setting for plane size

--- settings.py
+++ settings.py
@@ -3,14 +3,13 @@ SCREEN_WIDTH      = 1280
 SCREEN_RESOLUTION = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 FPS = 60
-SIMULATION_RUNTIME = 99999999 # in seconds
+SIMULATION_RUNTIME = 60 # in seconds
 USE_GUI = True
 
 PLANE_POLIKARPOV_I_16 = {
-    "SPRITE" : "assets/Steve_elytra.png", 
-    # "SPRITE" : "assets/sprite_republican_i16.png", 
+    "SPRITE" : "assets/sprite_republican_i16.png", 
     "MASS" : 1200,
-    "ENGINE_FORCE" : 300,
+    "ENGINE_FORCE" : 300,   
     "AGILITY" : 100, 
     "C_DRAG" : 0.5,
     "C_LIFT" : 15,
@@ -21,7 +20,8 @@ PLANE_POLIKARPOV_I_16 = {
     "INIT_THROTTLE" : 100,
     "INIT_PITCH" : 0, 
     "INIT_V" : (100.0, 0.0),
-    "INIT_POS" : (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    "INIT_POS" : (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2),
+    "SIZE" : (24,13)
 }
 
 BALLOON = {
@@ -36,6 +36,6 @@ BALLOON = {
                  "assets/cool_joost.png", 
                  "assets/max.png", 
                  "assets/igor.png"],
-    "SIZE" : 50,
+    "SIZE" : 20,
     "BALLOON_COUNT" : 10
 }
